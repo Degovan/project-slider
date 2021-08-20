18a0dab87e041c5b2b999656a32aa23aceb699d5
Mohammad Sahrullah <sahrul-dev@github.com>
2021-08-20 22:54:18 +0700

    upload using git bash

---
+++ resources/views/home.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>No Title</title>
+		<!-- google font link -->
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<!-- css stylesheet -->
+		<link rel="stylesheet" href="../css/main.css">
+		<link rel="stylesheet" href="../css/all.css">
+		<link rel="stylesheet" href="../css/solid.css">
+		<link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.css" crossorigin>
+		<link rel="stylesheet" href="https://unpkg.com/flickity-fullscreen@1/fullscreen.css" crossorigin>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+	<body>
+		<!-- header -->
+		<header class="header" id="header">
+			<div class="head-body container">
+				<!-- title or icon img -->
+				<div class="title-head icon-head">
+					<h2>No Title</h2>
+				</div>
+				<!-- bars toggle menu-->
+				<button class="btn btn-bar"><i class="fas fa-th"></i></button>
+				<!-- menu list head -->
+				<div class="menu-head">
+					<div class="menu-head-body">
+						<a href="#">Menu 1</a>
+						<a href="#">Menu 2</a>
+						<a href="#">Menu 3</a>
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- end of header -->
+		<!-- section main -->
+		<section class="main" id="main">
+			<div class="main-body">
+			<div class="carousel" data-flickity='{  "wrapAround": true  }'>
+				<div class="carousel-cell">
+					<div class="carousel-content">
+						<div class="title-cell">
+							<h2>Hello.</h2>
+						</div>
+						<br>
+						<div class="content-cell">
+							<p>Lorem, ipsum dolor sit amet consectetur adipisicing, elit. Recusandae iusto necessitatibus soluta, aliquam voluptate a quos excepturi quis ab ducimus reiciendis sunt maiores sequi alias, dolore laborum. Officia, voluptas nisi.</p>
+						</div>
+					</div>
+				</div>
+				<div class="carousel-cell">
+					<div class="carousel-content">
+						<div class="title-cell">
+							<h2>Hello.</h2>
+						</div>
+						<br>
+						<div class="content-cell">
+							<p>Lorem, ipsum dolor sit amet consectetur adipisicing, elit. Recusandae iusto necessitatibus soluta, aliquam voluptate a quos excepturi quis ab ducimus reiciendis sunt maiores sequi alias, dolore laborum. Officia, voluptas nisi.</p>
+						</div>
+					</div>
+				</div>
+				<div class="carousel-cell">
+					<div class="carousel-content">
+						<div class="title-cell">
+							<h2>Hello.</h2>
+						</div>
+						<br>
+						<div class="content-cell">
+							<p>Lorem, ipsum dolor sit amet consectetur adipisicing, elit. Recusandae iusto necessitatibus soluta, aliquam voluptate a quos excepturi quis ab ducimus reiciendis sunt maiores sequi alias, dolore laborum. Officia, voluptas nisi.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		</section>
+		<!-- end of section -->
+		<!-- footer -->
+		<footer class="footer" id="footer">
+			<section class="footer-body">
+				
+			</section>
+		</footer>
+		<!-- end of footer -->
+	</body>
+
+	<!-- javascript  -->
+	<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.js"></script>
+	<script src="https://unpkg.com/flickity-fullscreen@1/fullscreen.js"></script>
+	<script src="../js/main.js"></script>
+</html>
