5e919bf49984c40f6ff52a40338ced05be6d267a
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2021-08-21 20:04:35 +0700

    style: change owl carousel dot button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,104 +1,109 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>No Title</title>
-		<!-- google font link -->
-		<link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<!-- css stylesheet -->
-		<link rel="stylesheet" href="../css/main.css">
-		<link rel="stylesheet" href="../css/all.css">
-		<link rel="stylesheet" href="../css/solid.css">
-		<link rel="stylesheet" href="../OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css">
-		<link rel="stylesheet" href="../OwlCarousel2-2.3.4/dist/assets/owl.theme.default.min.css">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-		<script src="../js/jquery.min.js"></script>
-	</head>
-	<body>
-		<!-- header -->
-		<header class="header" id="header">
-			<div class="head-body container">
-				<!-- title or icon img -->
-				<div class="title-head icon-head">
-					<h2>No Title</h2>
-				</div>
-				<!-- bars toggle menu-->
-				<button class="btn btn-bar"><i class="fas fa-th"></i></button>
-				<!-- menu list head -->
-				<div class="menu-head">
-					<div class="menu-head-body">
-						<a href="#">Menu 1</a>
-						<a href="#">Menu 2</a>
-						<a href="#">Menu 3</a>
-					</div>
-				</div>
-			</div>
-		</header>
-		<!-- end of header -->
-		<!-- section main -->
-		<section class="main" id="main">
-			<div class="main-body">
-				<div class="owl-carousel owl-theme">
-					<div class="item-carousel" data-dot="<button class='countDot'></button>">
-						<div class="owl-wrap-text">
-							<div class="title-text">
-								<h2>No Title</h2>
-							</div>
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto blanditiis natus quo aut possimus dolor incidunt, similique provident placeat consequuntur temporibus ipsum voluptas neque nihil nulla vel beatae sit sapiente.</p>
-						</div>
-					</div>
-					<div class="item-carousel" data-dot="<button class='countDot'></button>">
-						<div class="owl-wrap-text">
-							<div class="title-text">
-								<h2>No Title</h2>
-							</div>
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto blanditiis natus quo aut possimus dolor incidunt, similique provident placeat consequuntur temporibus ipsum voluptas neque nihil nulla vel beatae sit sapiente.</p>
-						</div>
-					</div>
-					<div class="item-carousel" data-dot="<button class='countDot'></button>">
-						<div class="owl-wrap-text">
-							<div class="title-text">
-								<h2>No Title</h2>
-							</div>
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto blanditiis natus quo aut possimus dolor incidunt, similique provident placeat consequuntur temporibus ipsum voluptas neque nihil nulla vel beatae sit sapiente.</p>
-						</div>
-					</div>
-					<div class="item-carousel" data-dot="<button class='countDot'></button>">
-						<div class="owl-wrap-text">
-							<div class="title-text">
-								<h2>No Title</h2>
-							</div>
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto blanditiis natus quo aut possimus dolor incidunt, similique provident placeat consequuntur temporibus ipsum voluptas neque nihil nulla vel beatae sit sapiente.</p>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="owl-navigation">
-				<div id="counter">1/4</div>
-				<div class="btn-navigation-wrap">
-				<button class="btn-nav customPrevBtn">	&#8592;</button>
-				<button class="btn-nav customNextBtn">	&#8594;</button>
-				</div>
-			</div>
-		</section>
-		<!-- end of section -->
-		<!-- footer -->
-		<footer class="footer" id="footer">
-			<section class="footer-body">
-				
-			</section>
-		</footer>
-		<!-- end of footer -->
-	</body>
-	<!-- javascript  -->
-	<script src="../OwlCarousel2-2.3.4/dist/owl.carousel.min.js"></script>
-	<script src="../js/main.js"></script>
-	<script>
 
-	</script>
-	<style>
-		
-	</style>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>No Title</title>
+    <!-- google font link -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- css stylesheet -->
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/all.css">
+    <link rel="stylesheet" href="../css/solid.css">
+    <link rel="stylesheet" href="../OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="../OwlCarousel2-2.3.4/dist/assets/owl.theme.default.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="../js/jquery.min.js"></script>
+</head>
+
+<body>
+    <!-- header -->
+    <header class="header" id="header">
+        <div class="head-body container">
+            <!-- title or icon img -->
+            <div class="title-head icon-head">
+                <h2>No Title</h2>
+            </div>
+            <!-- bars toggle menu-->
+            <button class="btn btn-bar"><i class="fas fa-th"></i></button>
+            <!-- menu list head -->
+            <div class="menu-head">
+                <div class="menu-head-body">
+                    <a href="#">Menu 1</a>
+                    <a href="#">Menu 2</a>
+                    <a href="#">Menu 3</a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- end of header -->
+    <!-- section main -->
+    <section class="main" id="main">
+        <div class="main-body">
+            <div class="owl-carousel owl-theme">
+                <div class="item-carousel" data-dot="<button class='countDot'></button>">
+                    <div class="owl-wrap-text">
+                        <div class="title-text">
+                            <h2>No Title</h2>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto blanditiis natus quo aut
+                            possimus dolor incidunt, similique provident placeat consequuntur temporibus ipsum voluptas
+                            neque nihil nulla vel beatae sit sapiente.</p>
+                    </div>
+                </div>
+                <div class="item-carousel" data-dot="<button class='countDot'></button>">
+                    <div class="owl-wrap-text">
+                        <div class="title-text">
+                            <h2>No Title</h2>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto blanditiis natus quo aut
+                            possimus dolor incidunt, similique provident placeat consequuntur temporibus ipsum voluptas
+                            neque nihil nulla vel beatae sit sapiente.</p>
+                    </div>
+                </div>
+                <div class="item-carousel" data-dot="<button class='countDot'></button>">
+                    <div class="owl-wrap-text">
+                        <div class="title-text">
+                            <h2>No Title</h2>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto blanditiis natus quo aut
+                            possimus dolor incidunt, similique provident placeat consequuntur temporibus ipsum voluptas
+                            neque nihil nulla vel beatae sit sapiente.</p>
+                    </div>
+                </div>
+                <div class="item-carousel" data-dot="<button class='countDot'></button>">
+                    <div class="owl-wrap-text">
+                        <div class="title-text">
+                            <h2>No Title</h2>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto blanditiis natus quo aut
+                            possimus dolor incidunt, similique provident placeat consequuntur temporibus ipsum voluptas
+                            neque nihil nulla vel beatae sit sapiente.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="owl-navigation">
+            <div id="counter">1/4</div>
+            <div class="btn-navigation-wrap">
+                <button class="btn-nav customPrevBtn"> &#8592;</button>
+                <button class="btn-nav customNextBtn"> &#8594;</button>
+            </div>
+        </div>
+    </section>
+    <!-- end of section -->
+    <!-- footer -->
+    <footer class="footer" id="footer">
+        <section class="footer-body">
+
+        </section>
+    </footer>
+    <!-- end of footer -->
+    <!-- javascript  -->
+    <script src="../OwlCarousel2-2.3.4/dist/owl.carousel.min.js"></script>
+    <script src="../js/main.js"></script>
+</body>
+
 </html>
